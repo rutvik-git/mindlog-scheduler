fd9dcd9da09e2253d2529f48fd579f0e42f1cda1
Rutvik Shah <17.rutvik@gmail.com>
2019-06-05 16:31:18 -0700

    app update

--- mindlog_scheduler.py
+++ mindlog_scheduler.py
@@ -3,6 +3,8 @@ from mooddata_fetch_and_store import *
 from notify_user import *
 from apscheduler.schedulers.blocking import BlockingScheduler
 from multiprocessing import Process, Queue
+import os
+from flask import Flask
 import logging
 log = logging.getLogger('apscheduler.executors.default')
 log.setLevel(logging.INFO)
@@ -29,11 +31,13 @@ def mood_data_scheduler():
     except (KeyboardInterrupt, SystemExit):
         sched.shutdown(wait=False)
 
+app = Flask(__name__)
+@app.route("/")
+def startup():
+    p1 = Process(target=user_data_scheduler)
+    p1.start()
+    p2 = Process(target=mood_data_scheduler)
+    p2.start()
+    return "Server Started"
 
-
-
-p1 = Process(target=user_data_scheduler)
-p1.start()
-
-p2 = Process(target=mood_data_scheduler)
-p2.start()
+app.run()
