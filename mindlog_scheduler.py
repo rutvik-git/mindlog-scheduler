386b8c187fd56cc8e1bc7564841558334d4d2d6f
Rutvik Shah <17.rutvik@gmail.com>
2019-06-05 14:56:36 -0700

    update file

--- mindlog_scheduler.py
+++ mindlog_scheduler.py
@@ -32,8 +32,8 @@ def mood_data_scheduler():
 
 
 
-# p1 = Process(target=user_data_scheduler)
-# p1.start()
+p1 = Process(target=user_data_scheduler)
+p1.start()
 
 p2 = Process(target=mood_data_scheduler)
 p2.start()
