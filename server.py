1eb37dc2a71f61507270f817adcf7f2b47b94d12
Rutvik Shah <17.rutvik@gmail.com>
2019-06-05 16:15:14 -0700

    Update server.py

--- server.py
+++ server.py
@@ -2,4 +2,15 @@ from os import environ
 from flask import Flask
 
 app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Book, MoodData, UserData
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+    
 app.run()
