43f5e9a56629dc60dee262bdd76a1bca174dae01
Rutvik Shah <17.rutvik@gmail.com>
2019-06-07 14:50:46 -0700

    improvements

--- app.py
+++ app.py
@@ -5,44 +5,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from multiprocessing import Process, Queue
 import os
 from flask import Flask
-import logging
-log = logging.getLogger('apscheduler.executors.default')
-log.setLevel(logging.INFO)
-fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
-h = logging.StreamHandler()
-h.setFormatter(fmt)
-log.addHandler(h)
-
-def user_data_scheduler():
-    sched = BlockingScheduler()
-    sched.add_job(user_data_update, 'interval', seconds=15)
-    try:
-        sched.start()
-    except (KeyboardInterrupt, SystemExit):
-        sched.shutdown(wait=False)
-
-def mood_data_scheduler():
-    sched = BlockingScheduler()
-    sched.add_job(mood_data_update, 'interval', seconds=15)
-    try:
-        sched.start()
-    except (KeyboardInterrupt, SystemExit):
-        sched.shutdown(wait=False)
-
-def all_users_notification_scheduler():
-    sched = BlockingScheduler()
-    sched.add_job(schedule_new_users, 'interval', minutes=1)
-    try:
-        sched.start()
-    except (KeyboardInterrupt, SystemExit):
-        sched.shutdown(wait=False)
 
 app = Flask(__name__)
 @app.route("/")
 def startup():
-    os.system("python app_test.py")
-
-    return("Server Up and Running")
+    os.system("python run.py")
+    # return("Server Up and Running")
 
 if __name__ == '__main__':
     app.run()
