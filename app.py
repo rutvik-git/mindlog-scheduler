8d5c8a186dc9804c551f0c6438dee4fd7790c495
Rutvik Shah <17.rutvik@gmail.com>
2019-06-07 15:53:47 -0700

    commit

--- app.py
+++ app.py
@@ -5,12 +5,48 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from multiprocessing import Process, Queue
 import os
 from flask import Flask
+import logging
+log = logging.getLogger('apscheduler.executors.default')
+log.setLevel(logging.INFO)
+fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+h = logging.StreamHandler()
+h.setFormatter(fmt)
+log.addHandler(h)
+
+def user_data_scheduler():
+    sched = BlockingScheduler()
+    sched.add_job(user_data_update, 'interval', minutes=1)
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        sched.shutdown(wait=False)
+
+def mood_data_scheduler():
+    sched = BlockingScheduler()
+    sched.add_job(mood_data_update, 'interval', minutes=1)
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        sched.shutdown(wait=False)
+
+def all_users_notification_scheduler():
+    sched = BlockingScheduler()
+    sched.add_job(schedule_new_users, 'interval', minutes=2)
+    try:
+        sched.start()
+    except (KeyboardInterrupt, SystemExit):
+        sched.shutdown(wait=False)
 
 app = Flask(__name__)
 @app.route("/")
 def startup():
-    os.system("python run.py")
-    # return("Server Up and Running")
+    p1 = Process(target=user_data_scheduler)
+    p1.start()
+    p2 = Process(target=mood_data_scheduler)
+    p2.start()
+    p3 = Process(target=all_users_notification_scheduler)
+    p3.start()
+    return("Server Up and Running")
 
 if __name__ == '__main__':
     app.run()
